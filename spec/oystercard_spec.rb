a36535fe401223adec048b305670e9538e2a170c
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-05-04 16:27:58 +0100

    Step 3, 4 & 5: Initialized balance to 0 and enabled topup method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,17 @@
+require 'oystercard'
+
 describe Oystercard do
-end 
+  describe '#initialize' do
+    it 'returns the 0 balance of a new Oyster account' do
+      oystercard = Oystercard.new
+      expect(oystercard.balance).to eq 0
+    end
+  end
+
+  describe '#topup()' do
+    it 'checks the oystercard is topped up' do
+      oystercard = Oystercard.new
+      expect{ oystercard.topup(20) }.to change{ oystercard.balance }.by 20
+    end
+  end
+end

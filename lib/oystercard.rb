012e083887e557a5b84f42816f3ac81765bbced6
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-05-05 15:47:29 +0100

    Step 8: Added touch_in and touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,11 @@
 class Oystercard
   MAX_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def topup(amount)
@@ -18,5 +19,12 @@ class Oystercard
     @balance -= fare
   end
 
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,7 +6,7 @@ describe Oystercard do
   describe '#initialize' do
     it 'returns the 0 balance of a new Oyster account' do
       oystercard = Oystercard.new
-      expect(oystercard.balance).to eq 0
+      expect(oystercard.balance).to eq (0)
     end
   end
 
@@ -15,16 +15,36 @@ describe Oystercard do
       oystercard = Oystercard.new
       maximum_balance = Oystercard::MAX_BALANCE
       oystercard.topup(maximum_balance)
-      expect { oystercard.topup(1) }.to raise_error "Balance cannot exceed #{Oystercard::git aMAX_BALANCE}"
+      expect { oystercard.topup(1) }.to raise_error "Balance cannot exceed #{Oystercard::MAX_BALANCE}"
     end
   end
 
-  it 'can deduct from the balance' do
-    expect{ subject.deduct(5) }.to change{ subject.balance }.by -5
+  describe '#deduct' do
+    it 'can deduct from the balance' do
+      expect{ subject.deduct(5) }.to change{ subject.balance }.by -5
+    end
+  end
+  
+  describe '#top_up' do
+    it 'can topup from the balance' do
+      expect{ subject.topup(5) }.to change{ subject.balance }.by 5
+    end
   end
 
-  it 'can topup from the balance' do
-    expect{ subject.topup(5) }.to change{ subject.balance }.by 5
+  describe '#touch_in' do
+    it 'turns in_journey? to true' do
+      oystercard = Oystercard.new
+      oystercard.touch_in
+      expect(oystercard.in_journey).to eq (true)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'turns in_journey? to false' do
+      oystercard = Oystercard.new
+      oystercard.touch_out
+      expect(oystercard.in_journey).to eq (false)
+    end
   end
 
 end
